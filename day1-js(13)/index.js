433a901be83bfceab7c62b1d4bcc9f8f8dc3839b
MayThinnKhine <developer.maythinn@gmail.com>
2021-11-27 21:59:03 +0630

    day1 js (13)

---
+++ day1-js(13)/index.js
@@ -0,0 +1,55 @@
+
+// var username = "John";
+// let age = 25;
+// const fruit = "apple";
+// console.log(username, age, fruit)
+
+// const username="John", age= 25;
+// console.log(username, age)
+
+// let fruit = "apple";
+// let myfruit = fruit;
+// let Myfruit = "mango";
+// console.log(myfruit, Myfruit);
+
+// if(true){
+//     const username = "John";
+//     console.log(username)
+// }
+
+// console.log(username)
+
+// data types
+
+// let num = 5;
+// console.log(1/0);
+// console.log("not a number" + 2);
+// console.log(num);
+
+// let username = "John";
+// let fruit = 'apple ' + username;
+// let stringTemplate = `hello ${username}`;
+// console.log(username, fruit, stringTemplate)
+
+// let num = 4 > 6;
+// console.log(num)
+// let num = null;
+// console.log(num)
+
+// console.log(typeof undefined)
+
+// array
+// const fruits = ["apple", " orange", "mango"]
+// console.log(fruits[2])
+
+// object
+// const person = { name: "John", age: 25 }
+// console.log(person.name)
+// console.log(person.age)
+
+// alert("hello");
+// prompt("hello", "js")
+// confirm('Are you a boss?');
+
+
+
