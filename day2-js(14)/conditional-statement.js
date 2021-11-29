894010e1be54b2cdacfbdd75ea141f59cb695f6c
MayThinnKhine <developer.maythinn@gmail.com>
2021-11-29 22:08:43 +0630

    day2-js(14)

---
+++ day2-js(14)/conditional-statement.js
@@ -0,0 +1,23 @@
+// let day = "Monday";
+// if(day === "Monday"){
+//     console.log("Today is Monday.");
+// }
+// else if(typeof day === "string"){
+//     console.log("This is other string.");
+// }
+// else{
+//     console.log("Today isn't Monday.");
+// }
+
+let myfruit = "Orange";
+switch(myfruit){
+    case "Apple":
+        console.log("This is Apple.");
+        break;
+    case "Orange":
+        console.log("This is Orange.");
+        break;
+    default:
+        console.log("Dafult fruit.")
+}
+
