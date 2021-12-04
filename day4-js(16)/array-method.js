c0af35b86eff39d5224c43f9ad5f3fa32fee26cd
MayThinnKhine <developer.maythinn@gmail.com>
2021-12-04 22:04:50 +0630

    day4-js(16)

---
+++ day4-js(16)/array-method.js
@@ -0,0 +1,54 @@
+// let fruits = ["Apple", "Orange", "Mango", "Pineapple"];
+// let fruits = ["Apple", "Orange", "Mango", "Pineapple"];
+// console.log(fruits.length)
+// let fruitsString = fruits.toString();
+// console.log(typeof fruitsString);
+// console.log(fruitsString, fruits);
+
+// let pushArr = fruits.push("Pineapple");
+// let popArr = fruits.pop();
+// let shiftArr = fruits.shift();
+// let unshiftArr = fruits.unshift("Pineapple");
+// let spliceArr = fruits.splice(2, 1, "Kiwi", "lemon")
+// console.log(fruits)
+
+// let colors = ["white", "Black"];
+// let result = colors.concat(fruits);
+// console.log(result);
+
+// let sliceArr = fruits.slice(3);
+// console.log(sliceArr)
+// let sortArr = fruits.sort()
+
+// let fruits = ["Apple", "Orange", "Mango", "Pineapple"];
+// let text = "";
+// function func(item, index){
+//     text += item + " ";
+// }
+// fruits.forEach(func)
+// console.log(text)
+
+// const array1 = [1, 4, 9, 16];
+// function myfunc(x){
+//     return x * 2;
+// }
+
+// const myfunc = x => x * 3;
+// const map1 = array1.map(myfunc);
+// console.log(map1, array1)
+
+// const fruits = [12, 16, 20, 43];
+// const result = fruits.filter(item => item > 16);
+// console.log(result);
+
+// const arr1 = [12, 16, 20, 43];
+// const found = arr1.find(item => item > 16);
+// console.log(found);
+
+const pets = ['cat', 'dog', 'bat'];
+// const result = pets.includes('dog');
+// const result = pets.indexOf('dog');
+const result = pets.join("");
+console.log(result);
+
+
---
+++ day4-js(16)/string-method.js
@@ -0,0 +1,10 @@
+let fruit = "           This is Apple.         ";
+// console.log(fruit.length)
+// console.log(fruit.lastIndexOf("l"));
+// console.log(fruit.search('Ap'));
+// console.log(fruit.replace("Apple", "Orange"));
+// console.log(fruit.toUpperCase());
+// console.log(fruit.toLowerCase());
+// let price = "Price is 100";
+// console.log(fruit.concat(price));
+console.log(fruit.trim(""))
